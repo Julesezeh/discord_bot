0fb9ffca271bb00109afc25810e29eb31459124d
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-06-09 08:02:10 +0100

    message receiver created

---
+++ app.py
@@ -0,0 +1,43 @@
+import websocket
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token_ = os.getenv("TOKEN")
+
+
+def send_json_request(ws, request):
+    ws.send(json.dumps(request))
+
+
+def receive_json_response(ws):
+    response = ws.recv()
+    if response:
+        return json.loads(response)
+
+
+ws = websocket.WebSocket()
+ws.connect("wss://gateway.discord.gg/?v=6&encoding=json")
+heartbeat_interval = receive_json_response(ws)["d"]
+token = token_
+payload = {
+    "op": 2,
+    "d": {
+        "token": token,
+        "intents": "513",
+        "properties": {"$os": "Linux", "$browser": "chrome", "$device": "pc"},
+    },
+}
+
+send_json_request(ws, payload)
+
+while True:
+    event = receive_json_response(ws)
+    try:
+        # print(event)
+        content = event["d"]["content"]
+        author = event["d"]["author"]["username"]
+        print(f"{author}: {content}")
+    except:
+        pass
