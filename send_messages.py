1a5883fbc9106036c27be141c641498028f6bb3d
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-06-10 19:01:00 +0100

    extra comments

--- send_messages.py
+++ send_messages.py
@@ -4,13 +4,16 @@ from dotenv import load_dotenv
 
 # Fetches environment variables
 load_dotenv()
+
 # This sends messages to specified discord servers using python requests
 channel_id = os.getenv("LE_SLIME_CHANNEL_ID")
 server_url = f"https://discord.com/api/v9/channels/{channel_id}/messages"
-print(server_url)
 auth_token = os.getenv("TOKEN")
 
+
+# specifies the message to be sent of the request
 payload = {"content": "BRozay"}
+# specifies the main header attribute
 header = {"Authorization": auth_token}
 
 req = requests.post(server_url, data=payload, headers=header)
