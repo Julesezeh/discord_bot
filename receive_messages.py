e3fd4400d85fa9f77a58fc816884841927a31f77
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-20 12:12:55 +0100

    message reciever

--- receive_messages.py
+++ receive_messages.py
@@ -17,11 +17,31 @@ def receive_json_response(ws):
         return json.loads(response)
 
 
+async def add_to_file(data):
+    try:
+        author = data["d"]["author"]["username"]
+        id = data["d"]["author"]["id"]
+        message = data["d"]["content"]
+        date = data["d"]["timestamp"]
+    except KeyError as e:
+        return str(e)
+    text = {
+        "user": {"username": author, "id": id},
+        "message": message,
+        "timestamp": date,
+    }
+    with await open("history.txt", "w+") as file:
+        file.append(text)
+        file.close()
+
+
 def heartbeat(ws, heartbeat_):
     print("Heartbeat begins")
     while True:
         time.sleep(heartbeat_)
         heartbeat_payload = {"op": 1, "d": "null"}
+        send_json_request(ws, heartbeat_payload)
+        print("heartbeat sent")
 
 
 async def await_close(ws):
@@ -33,6 +53,11 @@ ws = websocket.WebSocket()
 # To get messages from the server
 ws.connect("wss://gateway.discord.gg/?encoding=json")
 
+heartbeat_interval = receive_json_response(ws)["d"]["heartbeat_interval"] / 1000
+
+hb = threading.Thread(target=heartbeat, args=(ws, heartbeat_interval))
+hb.start()
+
 token = token_
 
 payload = {
@@ -43,11 +68,8 @@ payload = {
     },
 }
 
-heartbeat_interval = receive_json_response(ws)["d"]["heartbeat_interval"]
-
 send_json_request(ws, payload)
 
-ws.ping()
 
 while True:
     event = receive_json_response(ws)
@@ -57,7 +79,13 @@ while True:
             content = event["d"]["content"]
             author = event["d"]["author"]["username"]
             print(f"{author}: {content}")
-            send_messages.send_message(content)
+            add_to_file(event)
+            if event["d"]["channel_id"] == "1076208583412297860":
+                print("valid ID")
+                send_messages.send_message(content)
+            else:
+                print(event["d"]["channel_id"])
+                print("Invalid ID")
     except Exception as e:
         print(e)
 # break
